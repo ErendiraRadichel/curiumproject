0297776171e83d390512d3bc372ebe223a42544c
ErendiraRadichel <106989313+ErendiraRadichel@users.noreply.github.com>
2022-06-16 14:37:49 -0500

    Creating a Backup

---
+++ APIHotProducts/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using APIHotProducts.Models;
+
+using APIHotProducts.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<APIHotProductsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("APIHotProductsContext") ?? throw new InvalidOperationException("Connection string 'APIHotProductsContext' not found.")));
+
+var app = builder.Build();
+
+// This will seed the data in the DB, only 2 entries.
+//If the DB is empty this will add two records automatically
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
