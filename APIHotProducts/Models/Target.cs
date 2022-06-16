3306be73eec44c2429f45d5026df9eb18c6a6776
ErendiraRadichel <106989313+ErendiraRadichel@users.noreply.github.com>
2022-06-16 14:41:03 -0500

    Adding target model

---
+++ APIHotProducts/Models/Target.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIHotProducts.Models
+{
+    public class Target
+    {
+        public int ID { get; set; }
+        public string Product { get; set; } = string.Empty;
+
+        public string Code { get; set; } = string.Empty;
+
+        public int LotNum { get; set; }
+
+        public string TargetType { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+    }
+}
