7e3217effa9f44c9680d947c083daa52570da140
ErendiraRadichel <106989313+ErendiraRadichel@users.noreply.github.com>
2022-06-16 14:43:03 -0500

    Adding data file

---
+++ APIHotProducts/Data/APIHotProductsContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIHotProducts.Models;
+
+namespace APIHotProducts.Data
+{
+    public class APIHotProductsContext : DbContext
+    {
+#pragma warning disable CS8618
+        public APIHotProductsContext (DbContextOptions<APIHotProductsContext> options)
+#pragma warning restore CS8618
+            : base(options)
+        {
+        }
+
+        public DbSet<APIHotProducts.Models.Target>? Target { get; set; }
+    }
+}
