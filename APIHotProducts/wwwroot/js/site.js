105fe24039afca6cbab2f25d1101843ba7a30bce
ErendiraRadichel <106989313+ErendiraRadichel@users.noreply.github.com>
2022-06-20 14:36:43 -0500

    Updating Project

--- APIHotProducts/wwwroot/js/site.js
+++ APIHotProducts/wwwroot/js/site.js
@@ -1,10 +1,48 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+//Displaying Forms fields
+function displayWarehouse() {
+    var x = document.getElementById("Warehouse");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
 
-function targetDescription() {
-    var code = document.getElementById('code').value;
-    if (code === CS)
-        //Need information about where the code changes
+function displayCyclotronS() {
+    var x = document.getElementById("CyclotronSpares");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayChemistry1() {
+    var x = document.getElementById("Chemistry1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayBombarding() {
+    var x = document.getElementById("Bombarding");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayChemistry2() {
+    var x = document.getElementById("Chemistry2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
