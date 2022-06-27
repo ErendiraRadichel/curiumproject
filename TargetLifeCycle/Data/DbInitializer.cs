004eda9e3fd26db628666f796dab93fc78c7ce5d
Erendira Radichel <erendira.radichel@gmail.com>
2022-06-27 14:25:49 -0500

    Trying new project

---
+++ TargetLifeCycle/Data/DbInitializer.cs
@@ -0,0 +1,56 @@
+﻿using TargetLifeCycle.Models;
+using System;
+using System.Linq;
+
+namespace TargetLifeCycle.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(CycleContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Targets.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var targets = new Target[]
+            {
+            new Target{TargetType="CS-30/IBA",Status="Warehouse"},
+            new Target{TargetType="CS-30/IBA",Status="Warehouse"},
+            new Target{TargetType="CS-30(flat)",Status="Cyclotron Spares"}
+            };
+            foreach (Target t in targets)
+            {
+                context.Targets.Add(t);
+            }
+            context.SaveChanges();
+
+            var warehouses = new Warehouse[]
+            {
+            new Warehouse{WarehouseCode="W00012",WarehouseLotNum=1256,WarehouseName="John D",WarehouseDate=DateTime.Parse("2022-01-01")},
+            new Warehouse{WarehouseCode="W00023",WarehouseLotNum=1256,WarehouseName="John D",WarehouseDate=DateTime.Parse("2022-01-01")},
+            new Warehouse{WarehouseCode="W00012",WarehouseLotNum=1256,WarehouseName="John D", WarehouseDate = DateTime.Parse("2022-01-01")},
+            };
+            foreach (Warehouse w in warehouses)
+            {
+                context.Warehouses.Add(w);
+            }
+            context.SaveChanges();
+
+            var cyclotrons = new Cyclotron[]
+            {
+            new Cyclotron{CyclotronName="Ed D",CyclotronDate=DateTime.Parse("2022-01-12")},
+            new Cyclotron{CyclotronName="Ed D",CyclotronDate=DateTime.Parse("2022-01-12")},
+            new Cyclotron{CyclotronName="Ed D",CyclotronDate=DateTime.Parse("2022-01-12")},
+            };
+            foreach (Cyclotron c in cyclotrons)
+            {
+                context.Cyclotrons.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ TargetLifeCycle/Models/Target.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TargetLifeCycle.Models
+{
+    public class Target
+    {
+        public int TargetID { get; set; }
+
+        [Display(Name = "Target Type")]
+        public string TargetType { get; set; }
+        public string Status { get; set; }
+
+        //References
+        public Warehouse Warehouse { get; set; }
+        public Cyclotron Cyclotron { get; set; }
+    }
+}
