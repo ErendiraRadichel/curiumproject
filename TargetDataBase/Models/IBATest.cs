d65f04bf03071313bb7d19635400f07dd1f7fe8e
Erendira Radichel <erendira.radichel@gmail.com>
2022-07-25 16:12:46 -0500

    Layout

---
+++ TargetDataBase/Models/IBATest.cs
@@ -0,0 +1,17 @@
+﻿namespace TargetDataBase.Models
+{
+    public class IBATest
+    {
+        public int ID { get; set; }
+
+        public int IBAID { get; set; }
+
+        public string TestType { get; set; }
+
+        public string Result { get; set; }
+
+        public string HNum { get; set; }
+
+        public IBA IBA { get; set; }
+    }
+}
